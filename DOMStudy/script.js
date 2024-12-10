bbdcf03d541bfcc3db570a63e56068fce5767626
GunnerBel <m.bela@alustudent.com>
2024-12-10 11:38:47 +0200

    DOM events

--- DOMStudy/script.js
+++ DOMStudy/script.js
@@ -1,11 +1,35 @@
-const button = document.querySelector('#btn')
-button.style.cursor = 'pointer';
+const h1 = document.createElement('h1'); 
+h1.textContent = 'Hello, Mitali!';          
+document.body.appendChild(h1);           
 
-button.addEventListener('mouseover', () => { 
-    document.querySelector('#greeting').textContent = 'Hello Bela!';
-});
+const but = document.createElement('button');
+but.textContent = 'Click Me';
+but.addEventListener('click', () => {
+    console.log('You clicked the button!');
+})
+document.body.appendChild(but);
 
-button.addEventListener('mouseout', () => { 
-    document.querySelector('#greeting').textContent = '';
-});
 
+const ul = document.createElement('ul');
+document.body.appendChild(ul);
+
+
+for(let i = 0; i < 10; i++) {
+    const li = document.createElement('li');
+    li.textContent = `Item ${i} `;
+    const button = document.createElement('button');
+    button.textContent = 'X';
+    button.addEventListener('click', () => {
+        li.remove();
+    })
+    
+    
+    li.appendChild(button);
+    ul.appendChild(li);
+}
+
+ul.addEventListener('click', (e) => {
+    if(e.target.tagName === 'LI') {
+        console.log('you clicked' + e.target.textContent);
+    }
+})
